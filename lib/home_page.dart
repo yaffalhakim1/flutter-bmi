ed045b28c62aec979030938cead9a01ce1fd6a62
yaffalhakim1 <yafi.tamfan08@gmail.com>
2021-06-09 15:02:04 +0700

    calculate BMI using provider

---
+++ lib/home_page.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:multi_provider/height_provider.dart';
+import 'package:multi_provider/weight_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Weight (kg) :',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            Consumer<WeightProvider>(
+              builder: (context, weightProvider, _) => Slider(
+                min: 1,
+                max: 100,
+                divisions: 100,
+                label: weightProvider.weight.round().toString(),
+                value: weightProvider.weight,
+                onChanged: (newValue) {
+                  newValue = newValue.roundToDouble();
+                  print('weight: $newValue');
+                  weightProvider.weight = newValue;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Height (cm) : ',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            Consumer<HeightProvider>(
+              builder: (context, heightProvider, _) => Slider(
+                min: 1,
+                max: 200,
+                divisions: 200,
+                label: heightProvider.height.round().toString(),
+                value: heightProvider.height,
+                activeColor: Colors.pink,
+                inactiveColor: Colors.pink.withOpacity(0.2),
+                onChanged: (newValue) {
+                  newValue = newValue.roundToDouble();
+                  print('Height: $newValue');
+                  heightProvider.height = newValue;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Consumer<WeightProvider>(
+              builder: (context, weightProvider, _) => Consumer<HeightProvider>(
+                builder: (context, heightProvider, _) => Text(
+                  'Your BMI :\n${(weightProvider.weight / (pow(heightProvider.height / 100, 2))).toStringAsFixed(1)}',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:multi_provider/height_provider.dart';
+import 'package:multi_provider/home_page.dart';
+import 'package:multi_provider/weight_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<WeightProvider>(
+      create: (context) => WeightProvider(),
+      child: ChangeNotifierProvider<HeightProvider>(
+        create: (context) => HeightProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        ),
+      ),
+    );
+  }
+}
